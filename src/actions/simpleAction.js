8f6128f44887c183b963fe2c992cd699ba9fe2ea
Matthias Hermann <matthias.hermann@iteratec.de>
2018-12-18 17:58:42 +0100

    Accept payload in simple action

--- src/actions/simpleAction.js
+++ src/actions/simpleAction.js
@@ -1,6 +1,6 @@
-export const simpleAction = () => dispatch => {
+export const simpleAction = (payload) => dispatch => {
     dispatch({
         type: 'SIMPLE_ACTION',
-        payload: 'result_of_simple_action'
+        payload: payload
     })
 }
