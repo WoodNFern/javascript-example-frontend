a6ef5d5f198301c7fd177190f5a8f3651102b76f
Matthias Hermann <matthias.hermann@iteratec.de>
2018-12-20 14:00:38 +0100

    Add simple page component

---
+++ src/components/page/WelcomeComponent.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import './Welcome.scss'
+
+class WelcomeComponent extends React.Component {
+
+    render() {
+        return (
+            <h1>Willkommen</h1>
+        );
+    }
+}
+
+export default WelcomeComponent;
